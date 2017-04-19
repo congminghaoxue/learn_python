364a0add7f6d4d2bb0c86f34282c4e5fce2fb096
zhoubo <congminghaoxue@outlook.com>
2017-04-19 17:06:53 +0800

    solution_level2 done

--- solution_level2.py
+++ solution_level2.py
@@ -22,14 +22,25 @@ def answer(n,b):
             digits.append('-')
         digits.reverse()
         return ''.join(digits)
-    k=len(n)
-    x= "".join((lambda x:(x.sort(),x)[1])(list(n)))
-    y = x[::-1]
-    print(x)
-    print(y)
-    x = int(x,3)
-    y= int(y,3)
-    print(x)
-    print(y)
-    print(int2base(y-x,b))
-answer('210022',3)
+    def foo(n,b):
+        k=len(n)
+        x= "".join((lambda x:(x.sort(),x)[1])(list(n)))
+        y = x[::-1]
+        x = int(x,b)
+        y= int(y,b)
+        z=int2base(y-x,b)
+        if k != len(z):
+            z = int(z) * 10**(k-len(z))
+        return z
+    lst = [n]
+    for v in lst:
+        tmp = foo(str(v),b)
+        if tmp in lst:
+            return len(lst) - lst.index(tmp)
+        else:
+            lst.append(tmp)
+
+if __name__ == '__main__':
+    foo = input('input string:')
+    bar = input('input base:')
+    print(answer(foo,bar))
