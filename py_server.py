dd6e4bc670b59201c723db6b038938f0e1798f63
zhoubo <congminghaoxue@outlook.com>
2017-06-28 22:27:23 +0800

    add txt mime

--- py_server.py
+++ py_server.py
@@ -16,6 +16,7 @@ Handler = http_server.SimpleHTTPRequestHandler
 Handler.extensions_map={
     '.manifest': 'text/cache-manifest',
     '.html': 'text/html',
+    '.txt': 'text/html',
     '.png': 'image/png',
     '.jpg': 'image/jpg',
     '.svg':'image/svg+xml',
