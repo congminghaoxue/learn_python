aa1ab2516c90af123e2bf70156501e95a5340339
zhoubo <congminghaoxue@outlook.com>
2017-09-15 17:50:21 +0800

    auto fix pep8 errors by sublime anaconda plugin

--- batch_cut_pic.py
+++ batch_cut_pic.py
@@ -1,41 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#function: 剪切更改图片尺寸大小
+# function: 剪切更改图片尺寸大小
 import os
 import os.path
-import sys, getopt, argparse
-from PIL import Image
+import argparse
+
 
 from change_pic_size_by_cut import CutImage
 
+
 def main():
-	argc = len(sys.argv)
-	cmdargs = str(sys.argv)
-	parser = argparse.ArgumentParser(description="Tool for batch cut the image")
-	parser.add_argument('-f', '--fromdir', required=True, help='the directory path of the input file')
-	parser.add_argument('-H', '--height',type=int, required=True, help='height of the output file')
-	parser.add_argument('-W', '--width',type=int, required=True, help='width of the output file')
-	parser.add_argument('-d', '--outdir', required=True, help='the directory of the output file')
-	parser.add_argument('-T', '--type', required=False, help='the type of the output file: jpeg, git, png ,etc')
-	args = parser.parse_args()
-	fromdir = args.fromdir
-	outdir = args.outdir
-	width = args.width
-	height = args.height
-	if args.type == None:
-		type = 'png'
-	else:
-		type = args.type
-	for file in os.listdir(fromdir):
-		if file == "desktop.ini":
-			continue
-		filein = os.path.join(fromdir, file)
-		fileout = os.path.join(outdir, file)
-		try:
-			CutImage(filein, fileout, width, height, type)
-		except Exception as e:
-			print(e)
-			continue
+
+    parser = argparse.ArgumentParser(description="Tool for batch cut the image")
+    parser.add_argument('-f', '--fromdir', required=True, help='the directory path of the input file')
+    parser.add_argument('-H', '--height', type=int, required=True, help='height of the output file')
+    parser.add_argument('-W', '--width', type=int, required=True, help='width of the output file')
+    parser.add_argument('-d', '--outdir', required=True, help='the directory of the output file')
+    parser.add_argument('-T', '--type', required=False, help='the type of the output file: jpeg, git, png ,etc')
+    args = parser.parse_args()
+    fromdir = args.fromdir
+    outdir = args.outdir
+    width = args.width
+    height = args.height
+    if args.type is None:
+        type = 'png'
+    else:
+        type = args.type
+    for file in os.listdir(fromdir):
+        if file == "desktop.ini":
+            continue
+        filein = os.path.join(fromdir, file)
+        fileout = os.path.join(outdir, file)
+        try:
+            CutImage(filein, fileout, width, height, type)
+        except Exception as e:
+            print(e)
+            continue
+
+
 if __name__ == '__main__':
-	main()
+    main()
--- locustio_example.py
+++ locustio_example.py
@@ -3,20 +3,25 @@
 
 from locust import HttpLocust, TaskSet
 
+
 def login(l):
-    l.client.post("/api/user/login", {"phone":"18500426633", "password":"111111"})
+    l.client.post("/api/user/login", {"phone": "18500426633", "password": "111111"})
+
 
 def index(l):
     l.client.get("/")
 
+
 def profile(l):
     l.client.get("/api/user/info")
 
+
 class UserBehavior(TaskSet):
-    tasks = {index:2, profile:1}
+    tasks = {index: 2, profile: 1}
 
     def on_start(self):
         login(self)
+
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
